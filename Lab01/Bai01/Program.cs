7a63a1ee467b861b0af8eafa4d208218b75f43b3
DalatCoder <hieuntctk42@gmail.com>
2020-01-31 17:29:15 +0700

    Hoan thanh Bai01

---
+++ Lab01/Bai01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bai01
+{
+  public class MyClass
+  {
+    public int A { get; set; }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var obj1 = new MyClass();
+      var obj2 = new MyClass();
+
+      obj1.A = 10;
+      obj2.A = 99;
+
+      Console.WriteLine($"Gia tri cua doi tuong obj1: {obj1.A}");
+      Console.WriteLine($"Gia tri cua doi tuong obj2: {obj2.A}");
+
+      Console.ReadLine();
+    }
+  }
+}
+
+/*
+* Nhan xet 2 doi tuong:
+* Thanh phan cua 2 doi tuong obj1 va obj2 giong nhau
+*/
