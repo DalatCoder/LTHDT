69d9fdb079e48cd2249accac4b0371a8970d9566
DalatCoder <hieuntctk42@gmail.com>
2020-02-01 17:31:26 +0700

    Bo sung xUnit test Lab05 - Bai02

---
+++ Lab05/test/MaTranTest.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using Lab05;
+using System;
+
+namespace test
+{
+  public class MaTranTest
+  {
+    [Fact]
+    public void ThaoTac_Cong2MaTranCungCap_TraVeMaTranMoi()
+    {
+      /*
+      * Khong the nhap gia tri cho ma tran
+      * ?????
+      */
+      var mt1 = new MaTran(2, 2);
+      var mt2 = new MaTran(2, 2);
+
+      for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+        {
+
+        }
+    }
+  }
+}
