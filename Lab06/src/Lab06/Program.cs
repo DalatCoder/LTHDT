0f52c5497addd89e961ecb1f7529d0b2580619db
DalatCoder <hieuntctk42@gmail.com>
2020-02-02 09:19:33 +0700

    Hoan thanh Lab06 - Bai01

---
+++ Lab06/src/Lab06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab06
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        TestQueue();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Co loi xay ra! {ex.Message}");
+      }
+    }
+
+    private static void TestQueue()
+    {
+      int max = 10;
+      var queue = new Queue(max);
+      for (int i = 0; i < max; i++)
+        queue.Enqueue(i);
+
+      Console.WriteLine("So phan tu trong hang doi: " + queue.Count);
+      var index = queue.Front;
+      while (!queue.IsEmpty)
+      {
+        Console.WriteLine(queue.Dequeue());
+      }
+    }
+  }
+}
