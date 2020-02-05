56be4ab793d370553f9cf2ae86dc6176c172efc1
DalatCoder <hieuntctk42@gmail.com>
2020-02-05 21:56:42 +0700

    Hoan thanh Lab10 - Bai03

---
+++ Lab10/Bai03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Bai03
+{
+  public class HinhTron
+  {
+    private readonly double r;
+    public HinhTron(double rad)
+    {
+      r = rad;
+    }
+
+    public double BanKinh { get => r; }
+    public double DienTich { get => Math.PI * Math.Pow(r, 2); }
+  }
+
+  public class HinhCau : HinhTron
+  {
+    private readonly double r;
+    public HinhCau(double r) : base(r)
+    {
+      this.r = r;
+    }
+
+    public new double BanKinh { get => r; }
+    public new double DienTich { get => 4 * Math.PI * Math.Pow(r, 2); }
+    public double TheTich { get => (4 / 3) * Math.PI * Math.Pow(r, 2); }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var tron = new HinhTron(2);
+      Console.WriteLine($"Dien tich hinh tron ban kinh 2: {tron.DienTich}");
+
+      var cau = new HinhCau(2);
+      Console.WriteLine($"Dien tich hinh cau co ban kinh 2: {cau.BanKinh}");
+      Console.WriteLine($"The tich hinh cau co ban kinh 2: {cau.TheTich}");
+    }
+  }
+}
